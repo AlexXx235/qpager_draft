e4eb3b7176ed474a029a77221d4cc0d43d33da2e
Danila Alekseev <alexxxkny@mail.ru>
2022-06-17 14:16:28 +0300

    Error trait implemented for Request error and some more ...

--- qpager_lib/src/lib.rs
+++ qpager_lib/src/lib.rs
@@ -1,7 +1,10 @@
+use std::error::Error;
+use std::fmt;
+
 use serde::{Serialize, Deserialize};
 use serde_json::{Value, Map};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Request {
     pub method: Method,
     pub params: Value
@@ -14,6 +17,18 @@ pub struct Responce {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct Event {
+    pub event_type: EventType,
+    pub params: Value
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum EventType {
+    NewPrivateMessage,
+    NewCharMessage
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Method {
     SignUp,
     LogIn,
@@ -22,7 +37,7 @@ pub enum Method {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum RequestResult {
-    Ok(()),
+    Ok(Value),
     Err(RequestError),
 }
 
@@ -31,7 +46,29 @@ pub enum RequestError {
     Auth(AuthError),
 }
 
+impl fmt::Display for RequestError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RequestError::Auth(err) => {
+                write!(f, "RequestError: authentication error: {}", err)
+            }
+        }
+    }
+}
+
+impl Error for RequestError {}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum AuthError {
     AlreadySignedUp,
+}
+
+impl fmt::Display for AuthError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AuthError::AlreadySignedUp => {
+                write!(f, "already signed up")
+            }
+        }
+    }
 }
